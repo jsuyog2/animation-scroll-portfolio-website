4734e09648d3bc3d6fe45f1361247f14b99fa135
jsuyog2 <jsuyog2@users.noreply.github.com>
2024-12-18 06:54:27 +0000

    deploy: 9949c773f026267271e6cd99ac26ef09be60c222

--- src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideAnimations(),]
-};
--- src/app/helper/enable-loader.guard.ts
+++
@@ -1,5 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-
-export const enableLoaderGuard: CanDeactivateFn<unknown> = (component: any, currentRoute, currentState, nextState: any) => {
-  return component.canDeactivate ? component.canDeactivate(nextState) : true;
-};
